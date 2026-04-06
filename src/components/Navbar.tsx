4705c46ed3279e87940874c45c1bb82f2eedc200
MurdoCodes <murdoc21daddie@gmail.com>
2026-04-06 11:14:20 +0800

    feat: add animation to logo and site name

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,13 +1,27 @@
+"use client";
+
 import { navLinksArray } from "@/const/const";
+import { motion } from "framer-motion";
 
 const Navbar = () => {
   return (
     <nav className="container mx-auto flex items-center justify-between">
       {/* Logo and Site Name */}
-      <div className="flex items-center gap-2">
+      <motion.div
+        initial={{ opacity: 0, x: -50 }}
+        animate={{ opacity: 1, x: 0 }}
+        transition={{
+          type: "spring",
+          stiffness: 100,
+          damping: 25,
+          delay: 0.3,
+          duration: 1.2,
+        }}
+        className="flex items-center gap-2"
+      >
         <img src="/logo_processed.svg" alt="Logo" className="w-10" />
         <h3 className="text-2xl font-bold">MurdoCodes</h3>
-      </div>
+      </motion.div>
 
       {/* Navigation Links */}
       <div className="flex">
