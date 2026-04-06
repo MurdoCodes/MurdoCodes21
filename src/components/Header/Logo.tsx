1bf3ea15d682c6f29dc9955525efffae1f026558
MurdoCodes <murdoc21daddie@gmail.com>
2026-04-06 13:41:29 +0800

    refactor: padding and margin auto

--- src/components/Header/Logo.tsx
+++ src/components/Header/Logo.tsx
@@ -18,7 +18,7 @@ const Logo = () => {
           delay: 0.3,
           duration: 1.2,
         }}
-        className="flex items-center gap-2"
+        className="flex items-center gap-3 group "
       >
         <img src="/logo_processed.svg" alt="Logo" className="w-10" />
         <h3 className="text-2xl font-bold hover:text-primary-hover hover:scale-[1.02] transition-colors duration-300 group">
