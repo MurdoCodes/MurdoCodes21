8ec812f01d64b937d0920e022e23e2b37b915170
MurdoCodes <murdoc21daddie@gmail.com>
2026-04-07 12:56:09 +0800

    feat: add contact form on web view

--- src/components/Header/HireMeButton.tsx
+++ src/components/Header/HireMeButton.tsx
@@ -1,6 +1,7 @@
 "use client";
 import { useState } from "react";
 import { motion } from "framer-motion";
+import ContactForm from "../ContactForm/ContactForm";
 
 const HireMeButton = () => {
   const [contactFormOpen, setContactFormOpen] = useState(false);
@@ -8,22 +9,28 @@ const HireMeButton = () => {
   const closeContactForm = () => setContactFormOpen(false);
 
   return (
-    <div className="hidden lg:flex">
-      <motion.button
-        initial={{ opacity: 0, scale: 0.8 }}
-        animate={{ opacity: 1, scale: 1 }}
-        transition={{
-          delay: 1.6,
-          duration: 0.8,
-          type: "spring",
-          stiffness: 100,
-          damping: 15,
-        }}
-        className="px-2 py-2 rounded-xl bg-linear-to-r from-primary to-primary-hover text-white font-bold hover:text-white hover:scale-[1.02] transition-all duration-500 md:w-30 md:h-10 text-sm md:text-base"
-      >
-        Hire Me
-      </motion.button>
-    </div>
+    <>
+      <div className="hidden lg:flex">
+        <motion.button
+          initial={{ opacity: 0, scale: 0.8 }}
+          animate={{ opacity: 1, scale: 1 }}
+          transition={{
+            delay: 1.6,
+            duration: 0.8,
+            type: "spring",
+            stiffness: 100,
+            damping: 15,
+          }}
+          className="px-2 py-2 rounded-xl bg-linear-to-r from-primary to-primary-hover text-white font-bold hover:text-white hover:scale-[1.02] transition-all duration-500 md:w-30 md:h-10 text-sm md:text-base"
+          onClick={() => {
+            openContactForm();
+          }}
+        >
+          Hire Me
+        </motion.button>
+      </div>
+      {contactFormOpen && <ContactForm closeContactForm={closeContactForm} />}
+    </>
   );
 };
 
