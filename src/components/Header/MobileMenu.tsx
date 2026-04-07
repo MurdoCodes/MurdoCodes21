559a13e76bf74fb4e362f5726da43c651e10062d
MurdoCodes <murdoc21daddie@gmail.com>
2026-04-07 11:21:56 +0800

    feat: add animations on mobile menu

--- src/components/Header/MobileMenu.tsx
+++ src/components/Header/MobileMenu.tsx
@@ -1,6 +1,7 @@
 "use client";
 import { useState } from "react";
-import { motion } from "framer-motion";
+import { motion, AnimatePresence } from "framer-motion";
+
 import { FiMenu, FiX } from "react-icons/fi";
 import { navLinksArray } from "@/const/const";
 
@@ -16,7 +17,18 @@ const MobileMenu = () => {
   return (
     <>
       {/* Mobile Menu Button */}
-      <div className="md:hidden flex items-center">
+      <motion.div
+        initial={{ opacity: 0, y: -50 }}
+        animate={{ opacity: 1, y: 0 }}
+        transition={{
+          type: "spring",
+          stiffness: 100,
+          damping: 25,
+          delay: 0.5,
+          duration: 1.2,
+        }}
+        className="md:hidden flex items-center"
+      >
         <motion.button
           whileTap={{ scale: 0.7 }}
           onClick={toggleMenu}
@@ -30,48 +42,43 @@ const MobileMenu = () => {
             <FiMenu className="h-6 w-6" />
           )}
         </motion.button>
-      </div>
+      </motion.div>
 
       {/* Mobile Menu List */}
-      {isOpen && (
-        <div
-          className="
+      <AnimatePresence>
+        {isOpen && (
+          <motion.div
+            initial={{ opacity: 0, height: 0 }}
+            animate={{ opacity: 1, height: "auto" }}
+            exit={{ opacity: 0, height: 0 }}
+            transition={{ duration: 0.3, ease: "easeInOut" }}
+            className="
+            overflow-hidden
+          bg-gray-900
             backdrop-blur-sm
             absolute
             z-40
             w-full
             top-full
             left-0
-            px-4
-            py-4
-            overflow-hidden
             md:hidden
-            text-left
             "
-        >
-          <nav
-            className="
+          >
+            <nav
+              className="
               flex
               flex-col
               items-start
-              text-left!
+              px-4
+              py-4
               "
-          >
-            {navLinksArray.map((link, index) => (
-              <motion.a
-                initial={{ opacity: 0, y: -20 }}
-                animate={{ opacity: 1, y: 0 }}
-                transition={{
-                  type: "spring",
-                  stiffness: 100,
-                  damping: 20,
-                  // delay: 0.7 + index * 0.2,
-                  duration: 1,
-                }}
-                key={index}
-                href={`#${link.href}`}
-                onClick={toggleMenu}
-                className="
+            >
+              {navLinksArray.map((link, index) => (
+                <a
+                  key={index}
+                  href={`#${link.href}`}
+                  onClick={toggleMenu}
+                  className="
                   relative 
                   text-white 
                   hover:text-primary-hover 
@@ -83,13 +90,14 @@ const MobileMenu = () => {
                   py-2
                   w-full
                 "
-              >
-                {link.name}
-              </motion.a>
-            ))}
-          </nav>
-        </div>
-      )}
+                >
+                  {link.name}
+                </a>
+              ))}
+            </nav>
+          </motion.div>
+        )}
+      </AnimatePresence>
     </>
   );
 };
--- src/components/Header/SocialLinks.tsx
+++ src/components/Header/SocialLinks.tsx
@@ -6,7 +6,7 @@ const SocialLinks = () => {
   return (
     <>
       {/* Social icons - Desktop */}
-      <div className="md:flex  hidden items-center space-x-4 gap-2">
+      <div className="md:flex hidden items-center space-x-4 gap-2">
         <motion.a
           initial={{ opacity: 0, scale: 0.8 }}
           animate={{ opacity: 1, scale: 1 }}
